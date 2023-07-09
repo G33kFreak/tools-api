3e2b22d706679b03d9e19a5bf5add3d29cf1c220
Ruslan Zaripov <zaripo.rus@gmail.com>
2023-07-09 14:09:58 +0200

    finish

--- toolsapi/settings.py
+++ toolsapi/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     'drf_yasg',
     'users',
@@ -61,7 +62,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True  
 
 ROOT_URLCONF = 'toolsapi.urls'
 
